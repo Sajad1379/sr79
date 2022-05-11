bff09246061f23d8b138459e79f288f7fb130c21
Sajad Rahmani <sajad.rahmani4180@gmail.com>
2022-05-12 01:16:06 +0430

    fix example

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -5,6 +5,7 @@ class Example extends Component{
     render(){
         return (
             <div className="container">
+                <h1>hello world</h1>
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
